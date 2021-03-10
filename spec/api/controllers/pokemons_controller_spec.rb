7335ed62d9a212ea143ae2def45e2af5cebd09db
davidbordeleau <davidbordeleau@hotmail.com>
2021-03-10 09:31:21 -0500

    fix specs for consistent remote spacing

--- spec/api/controllers/pokemons_controller_spec.rb
+++ spec/api/controllers/pokemons_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Api::PokemonsController, type: :controller do
   describe '.show' do
-  	subject { get :show, params: params }
+    subject { get :show, params: params }
 
     context 'with an non existing id' do
       let(:params) do
@@ -15,14 +15,14 @@ describe Api::PokemonsController, type: :controller do
     end
 
     context 'with an existing id' do
-    	let!(:pokemon) { Pokemon.create(name: 'a pokemon') }
+      let!(:pokemon) { Pokemon.create(name: 'a pokemon') }
 
       let(:params) do
         { id: pokemon.id }
       end
 
       it 'finds the pokemon' do
-      	expect(subject.status).to eq(200)
+        expect(subject.status).to eq(200)
       end
     end
   end
@@ -30,37 +30,37 @@ describe Api::PokemonsController, type: :controller do
   describe '.create' do
     it 'finds the route' do
       expect(post: '/api/pokemons?name=poke').to route_to(controller: 'api/pokemons', action: 'create', name: 'poke')
-	  end
+    end
 
-   	it 'creates a pokemon' do
-	  	post :create, params: { name: 'new created name', type_one: 'new type' }
-	  	expect(Pokemon.last.name).to eq('new created name')
-		end
+    it 'creates a pokemon' do
+      post :create, params: { name: 'new created name', type_one: 'new type' }
+      expect(Pokemon.last.name).to eq('new created name')
+    end
   end
 
   describe '.update' do
-		let!(:pokemon) { Pokemon.create(name: 'a pokemon') }
+    let!(:pokemon) { Pokemon.create(name: 'a pokemon') }
     it 'finds the route' do
       expect(post: '/api/pokemons?name=poke').to route_to(controller: 'api/pokemons', action: 'create', name: 'poke')
-	  end
+    end
 
-	  it 'updates the pokemon' do
-	  	expect(pokemon.name).to eq('a pokemon')
-	  	put :update, params: { id: pokemon.id, name: 'new name', type_one: 'new type' }
-  		pokemon.reload
-  		expect(pokemon.name).to eq('new name')
-		end
+    it 'updates the pokemon' do
+      expect(pokemon.name).to eq('a pokemon')
+      put :update, params: { id: pokemon.id, name: 'new name', type_one: 'new type' }
+      pokemon.reload
+      expect(pokemon.name).to eq('new name')
+    end
   end
 
   describe '.destroy' do
-  	context 'test routing' do
-  		let!(:pokemon) { Pokemon.create(name: 'a newly created pokemon') }
+    context 'test routing' do
+      let!(:pokemon) { Pokemon.create(name: 'a newly created pokemon') }
 
-  	  it 'deletes the pokemon' do
-  	  	expect(Pokemon.find_by(name: 'a newly created pokemon')).to_not be_nil
-  	  	post :destroy, params: { id: pokemon.id }
-				expect(Pokemon.find_by(name: 'a newly created pokemon')).to be_nil
-  		end
+      it 'deletes the pokemon' do
+        expect(Pokemon.find_by(name: 'a newly created pokemon')).to_not be_nil
+        post :destroy, params: { id: pokemon.id }
+        expect(Pokemon.find_by(name: 'a newly created pokemon')).to be_nil
+      end
     end
   end
 end
